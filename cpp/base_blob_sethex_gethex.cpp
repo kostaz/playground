9d85e4e4e4184811fd0bcc23f1bfc3ceff17aae5
Kosta Zertsekel <zertsekel@gmail.com>
2018-04-03 16:16:28 +0300

    Fix misspellings
    Signed-off-by: Kosta Zertsekel <zertsekel@gmail.com>

--- cpp/base_blob_sethex_gethex.cpp
+++ cpp/base_blob_sethex_gethex.cpp
@@ -122,22 +122,22 @@ void base_blob<BITS>::SetHex(const char* psz)
  */
 int main()
 {
-    // 2-byte array (16 bits, 2 charactres)
+    // 2-byte array (16 bits, 2 characters)
     base_blob<16> bb16;
     bb16.SetHex("abcd");
     std::cout << bb16.GetHex() << std::endl;
 
-    // 4-byte array (32 bits, 8 charactres)
+    // 4-byte array (32 bits, 8 characters)
     base_blob<32> bb32;
     bb32.SetHex("abcd1234");
     std::cout << bb32.GetHex() << std::endl;
 
-    // 20-byte array (160 bits, 40 charactres)
+    // 20-byte array (160 bits, 40 characters)
     base_blob<160> bb160;
     bb160.SetHex("1234567890abcdefa0b1c2d3e4f50987654321eb");
     std::cout << bb160.GetHex() << std::endl;
 
-    // 32-byte array (256 bits, 64 charactres)
+    // 32-byte array (256 bits, 64 characters)
     base_blob<256> bb256;
     bb256.SetHex("1234567890abcdefa0b1c2d3e4f50987654321eb1234567890abcdefa0b1c2d3");
     std::cout << bb256.GetHex() << std::endl;
